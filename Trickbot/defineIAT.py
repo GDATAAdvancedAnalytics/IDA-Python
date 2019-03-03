a4be4f49c336d57b0a797b9078f1e0da0c5fafe0
Robert Michel <robert.michel@rub.de>
2019-03-03 20:30:17 +0100

    Initial commit of Trickbot scripts for defining the IAT and decrypting all strings

---
+++ Trickbot/defineIAT.py
@@ -0,0 +1,247 @@
+#
+# This script has been written for the Trickbot sample with sha256 8F590AC32A7C7C0DDFBFA7A70E33EC0EE6EB8D88846DEFBDA6144FADCC23663A
+# Tested with IDA 6.9
+#
+
+from idaapi import *
+import idautils
+
+g_stringArrayStart = 0x00427C1C #beginning of the array of pointers to the base64 encoded strings
+keyAddress = 0x0042A050 #address of the base64 alphabet
+g_key = list(GetString(Dword(keyAddress), -1, ASCSTR_C)) # e.g. 'azcNwZVEXQybv2s6Lg9tjkxOr4uMKA1SWJHoBqTm/f0Fn8peCI7hiU5GRd+YD3Pl'
+
+g_ImportStructBase = 0x0042A058 #Import Table of the runtime genereated Import Address Table (IAT)
+g_IATptr = 0x0042A648 #address of where the pointer to the IAT is stored. Used as base when referencing the IAT
+
+class crypt:
+  def __getDWORD(self, instr):
+    res = []
+
+    for el in instr:
+      for i in range(len(g_key)):
+        if el == g_key[i]:
+          res.append(i)
+          break
+
+    while len(res) < 4: res.append(0) #pad DWORD with zeros
+    return res
+      
+  def __unscramble(self, instr):
+    if len(instr) != 4:
+      print "[-] Error: input in \"__unscramble\" is garbage"
+      return ""
+
+    a0 = (4 * instr[0] + ((instr[1] >> 4) & 3)) & 0xff
+    a1 =  (16 * instr[1] ^ (instr[2] >> 2) & 0xF) &0xff
+    a2 =  (instr[3] + (instr[2] << 6)) &0xff
+    return "".join([chr(a0), chr(a1), chr(a2)])
+
+
+  def decrypt(self, inputString):
+    res = ""
+    #unscramble string in blocks of 4 bytes
+    for i in range(0, len(inputString),4):
+      base64Foo = self.__getDWORD(inputString[i:i+4])
+      res += self.__unscramble(base64Foo)
+    return res
+
+def getStringFromPointer(offset):
+  addr = g_stringArrayStart + (offset * 4)
+  return GetString(Dword(addr), -1, ASCSTR_C)
+
+
+def getIATasList():
+  """
+  Take information at g_ImportStructBase, apply reverse engineered structure on it to get all import names
+  return list of IAT entries in their order
+  """
+  base = g_ImportStructBase
+  cryptHelper = crypt()
+  
+  IAT = [0 for i in range(130)] # c++: new(0x208u) bytes;
+  for i in range(8): #8 elements in array
+    dllNameOffset =  Dword(base)
+    funcNameArrayStart =  Dword(base + 4)
+    funcNameArrayEnd =  Dword(base + 8)
+    IATOffset =  Dword(base + 12) / 4 #IAT during runtime dynmically on heap
+    
+    for ii in range(funcNameArrayStart, funcNameArrayEnd + 1): #check in code is ">=", not ">", so the end is +1
+      st = getStringFromPointer(ii)
+      dec = cryptHelper.decrypt(st)
+      IAT[IATOffset] = dec.replace('\x00',"") #remove zero byte padding from strings
+      IATOffset += 1
+    base += 16
+  return IAT
+
+def setIATasLocalType(IATlist):  
+  """  
+  Create one huge IAT structure, import it as Local Type and set the type to g_IATptr
+  """  
+  funcDefinition = "struct IAT {"  
+  for IATentry in IATlist:
+    if IATentry != 0:
+      funcDefinition +=  "%s *%s;" % (IATentry, IATentry)
+  funcDefinition += "};"
+
+  if SetLocalType(-1, funcDefinition, 0) == 0:
+    print "[-] Error setting IAT as local type"
+    return
+  
+  if not SetType(g_IATptr, "IAT* IAT;"):
+    print "[-] Error when setting IAT type"
+
+
+def traceUsage(addr, register, steps):
+  """
+  Given a start address, a register which holds a value and the number of steps,
+  this function disassembles forward #steps instructions and traces the value of <register>
+  until it is used in a call instruction. It then returns the offset added to <register> and the address of the call
+  Note that this tracing is very basic and does neither handle multiple registers at the same time nor any other modification than adding constants
+  e.g.:
+  00401622 mov eax, g_IAT           //start at addr = 0x00401622, register = "eax"
+  00401627 mov ecx, [eax+0Ch]       //trace ecx, forget eax from now on. Save offset "0x0C"
+  0040162A push    edx              //ignore
+  0040162B call ecx                 //return offset 0x0c and address 0x0040162B
+  """
+  potentialOffset = -1
+  localRegister = register
+
+  for step in range(steps):
+    addr = NextHead(addr)
+    dis = GetMnem(addr)
+
+    if dis == 'mov' and localRegister in GetOpnd(addr,1): #look for e.g."mov eax, [<register>+1CCh]"
+      potentialOffset = GetOpnd(addr,1)
+      if potentialOffset[0] != '[' or potentialOffset[-1] != ']': #"[<register>+1CCh]"
+        continue
+      potentialOffset = potentialOffset[1:-1] #"<register>+1CCh"
+
+      if '+' in potentialOffset: #we might have had "mov ecx, [eax]", so there is no plus
+        potentialOffset = potentialOffset.split(register+'+')[1] # "1CCh"
+      else:
+        potentialOffset = "0"
+
+      if potentialOffset.endswith('h'):
+        potentialOffset = int(potentialOffset[:-1], 16) / 4 #"1cc"
+      else:
+        potentialOffset = int(potentialOffset) / 4
+
+      localRegister = GetOpnd(addr,0) #get new register to search for upcoming call-instruction    
+
+    elif dis == 'call' and GetOpnd(addr,0) == localRegister:
+      return potentialOffset, addr
+  
+  if potentialOffset != -1:
+    print "[-] Error: Got potentialOffset %s but no corresponding call - maybe increase the steps range?" % (str(potentialOffset))
+  return -1, -1 #errör
+
+def helper_getTinfoOfFuncName(funcName):
+  """
+  Return the tinfo-object for a given function name
+  Success/Fail is transported via second return value
+  """
+  sym = idaapi.til_symbol_t()
+  sym.til = idaapi.cvar.idati
+  sym.name = funcName
+  tinfo = idaapi.tinfo_t()
+
+  namedType = idaapi.get_named_type(sym.til, sym.name, 0)
+
+  if namedType == None:
+    print '[-] Error: Could not find %s' % (sym.name)
+    return tinfo, False
+    
+  tinfo.deserialize(sym.til, namedType[1], namedType[2])
+
+  return tinfo, True
+
+def helper_getFunctionDefinitionString(funcName):
+  """
+  Return function definition of funcName as string. Empty string on error
+  e.g. helper_getFunctionDefinitionString("SetCurrentDirectoryW")
+  returns "typedef BOOL __stdcall SetCurrentDirectoryW(LPCWSTR lpPathName);"
+  """
+  tinfo, success = helper_getTinfoOfFuncName(funcName)
+  if not success:
+    print "[-] Error: Cannot find tinfo for function name %s" % (funcName)
+    return ""
+    
+  #cast tinfo to string in order to parse it
+  funcStr = tinfo.__str__()
+  funcStr = funcStr.split('(')
+  funcStr = "typedef " + funcStr[0] + " " + funcName + " (" + funcStr[1] + ";"
+  return funcStr
+
+def importAllFunctionDefinitionsAsLocalType(IATlist):
+  """
+  This function iterates over all entries of IATlist and creates and imports the function definitions for later use when creating the IAT structure.  
+  e.g.: Before you can create a function pointer structure with "SetCurrentDirectoryW *SetCurrentDirectoryW;"
+  you need to define "typedef BOOL __stdcall SetCurrentDirectoryW(LPCWSTR lpPathName);" first.
+  """
+  for funcStr in IATlist:
+    funcDefinition = helper_getFunctionDefinitionString(funcStr)
+    
+    if funcDefinition == "":
+      print "[-] Error: Cannot get function definition for function name %s" % (funcStr)
+      continue
+  
+    if SetLocalType(-1, funcDefinition, 0) == 0:
+      print "[-] Error when setting local type %s. Does it already exist?" % (funcStr)
+
+
+def setFunctionInformation(funcName, callAddress):
+  """
+  This function takes the function index offset for the IAT and the address, where the call to the IAT is made.
+  It tells IDA to define the correct definition to the call, including local variables and comments for the disassembler and the decompiler
+  """  
+  tinfo, success = helper_getTinfoOfFuncName(funcName)
+  if not success:
+    print "[-] Error: Cannot find tinfo for function name %s" % (funcName)
+    return False
+
+  errorCode = idaapi.apply_callee_tinfo(callAddress, tinfo)
+  success = idaapi.set_op_tinfo2(callAddress, 0, tinfo)
+  if errorCode != None or not success:
+    print "[-] Error when setting function information for %s at %s" % (sym.name, hex(int(callAddress)))
+    return False
+  return True
+
+
+def importTypeLibraries():
+  """
+  Import type libraries needed for function definitions
+  """  
+  if add_til("wdk8_um") != 1 or add_til("mssdk_win7") != 1:
+    return False
+  return True
+
+
+if __name__ == "__main__":
+  #ensure that all needed type libaries are loaded
+  if not importTypeLibraries():
+    print "[-] Error importing type libaries"
+    raise Exception("Type lib import failed")
+
+  IATlist = getIATasList()  
+  
+  importAllFunctionDefinitionsAsLocalType(IATlist)
+  
+  setIATasLocalType(IATlist)
+
+  #propagate type information for each address where the IAT is used
+  for i in idautils.XrefsTo(g_IATptr,0):
+    addr = i.frm
+    dis = GetMnem(addr)
+    if not 'mov' == dis:
+      print "[-] Crap, no mov at %s" % (hex(int(addr)))
+      continue
+    
+    offset, addrOfCall = traceUsage(addr, GetOpnd(addr, 0), 18)
+    if offset == -1 or addrOfCall == -1:
+      print "[-] Error: Trace unsuccessful at %s" % (hex(int(addr)))
+    else:
+      funcName = IATlist[offset]
+      if not setFunctionInformation(funcName, addrOfCall):
+        print "[-] Error with offset %s at %s with call on %s" % (str(offset), hex(int(addr)), hex(int(addrOfCall)))
+      else:
+        print "[+] Success with offset %s at %s with call on %s" % (str(offset), hex(int(addr)), hex(int(addrOfCall)))
---
+++ Trickbot/stringDecryption.py
@@ -0,0 +1,180 @@
+#
+# This script has been written for the Trickbot sample with sha256 8F590AC32A7C7C0DDFBFA7A70E33EC0EE6EB8D88846DEFBDA6144FADCC23663A
+# Tested with IDA 6.9
+#
+
+import idautils
+
+g_stringArrayStart = 0x00427C1C
+keyAddress = 0x0042A050
+
+#key from binary (first element of blob)
+g_key = list(GetString(Dword(keyAddress), -1, ASCSTR_C)) # == 'azcNwZVEXQybv2s6Lg9tjkxOr4uMKA1SWJHoBqTm/f0Fn8peCI7hiU5GRd+YD3Pl'
+
+class crypt:
+    def __getDWORD(self, instr):
+        global g_key
+        res = []
+
+        for el in instr:
+            for i in range(len(g_key)):
+                if el == g_key[i]:
+                    res.append(i)
+                    break
+
+        while len(res) < 4: res.append(0) #pad DWORD with zeros
+        return res
+        
+    def __unscramble(self, instr):
+        if len(instr) != 4:
+            print "[-]Error, input in \"unscramble\" is garbage"
+            return ""
+
+        a0 = (4 * instr[0] + ((instr[1] >> 4) & 3)) & 0xff
+        a1 =  (16 * instr[1] ^ (instr[2] >> 2) & 0xF) &0xff
+        a2 =  (instr[3] + (instr[2] << 6)) &0xff
+        return "".join([chr(a0), chr(a1), chr(a2)])
+
+
+    def decrypt(self, inputString):
+        res = ""
+        #unscramble string in blocks of 4 bytes
+        for i in range(0, len(inputString),4):
+            base64Foo = self.__getDWORD(inputString[i:i+4])
+            res += self.__unscramble(base64Foo)
+        return res
+
+
+def getOperandBackwards(addr, maxrange=5):
+    """
+    Disassemble backwards from <addr> up to <maxrange> instructions
+    Search for a "push <const>" instruction and return the pushed value
+    """
+    ret = -1
+    for i in range(maxrange):
+        addr = PrevHead(addr)
+        dis = GetMnem(addr)
+
+        if dis == "push":
+            operand = GetOpnd(addr, 0)
+            if operand.endswith('h'):
+                ret = int(operand[:-1], 16) #cut off 'h' at the end, then convert to int from hex
+                break
+            elif operand.isdigit():
+                ret = int(operand, 10)
+                break
+    return ret
+
+def getStringFromPointer(offset):
+    global g_stringArrayStart
+    addr = g_stringArrayStart + (offset * 4)
+    return GetString(Dword(addr), -1, ASCSTR_C)
+
+def printIAT():
+    base = 0x0042A058
+    cryptHelper = crypt()
+    IAT = [0 for i in range(0x208)] # c++: new(0x208u);
+    for i in range(8): #8 elements in array
+        dllNameOffset =  Dword(base)
+        funcNameArrayStart =  Dword(base + 4)
+        funcNameArrayEnd =  Dword(base + 8)
+        IATOffset =  Dword(base + 12) / 4 #IAT during runtime dynmically on heap
+    
+        #print DLL name
+        st = getStringFromPointer(dllNameOffset)
+        print hex(int(base)), dllNameOffset, st, cryptHelper.decrypt(st)
+
+        #print imported function names
+        for ii in range(funcNameArrayStart, funcNameArrayEnd + 1): #check in code is ">=", not ">", so the end is +1
+            st = getStringFromPointer(ii)
+            dec = cryptHelper.decrypt(st)
+            if IAT[IATOffset] != 0: print "AAAA",IATOffset #TODO proper error handling
+            IAT[IATOffset] = dec.replace('\x00',"") #remove zero byte padding from strings
+            IATOffset += 1
+            print ii, st, dec
+        base += 16
+    
+    print "----------------"
+    print "struct IAT {"
+    for i in range(len(IAT)):
+        if IAT[i] != 0:
+            print "%s *%s;" % (IAT[i], IAT[i])
+    print "}"
+
+def setCommentToDecompilation(comment, address):
+    #Works in IDA 6.9 - May not work in IDA 7
+    #see https://www.hex-rays.com/products/decompiler/manual/sdk/hexrays_8hpp_source.shtml used structures, const and functions
+    cfunc = idaapi.decompile(address)
+    
+    #get the line of the decompilation for this address
+    eamap = cfunc.get_eamap()
+    decompObjAddr = eamap[address][0].ea
+
+    #get a ctree location object to place a comment there
+    tl = idaapi.treeloc_t()
+    tl.ea = decompObjAddr
+    
+    commentSet = False
+    #since the public documentation on IDAs APIs is crap and I don't know any other way, we have to brute force the item preciser
+    #we do this by setting the comments with different idaapi.ITP_* types until our comment does not create an orphaned comment
+    for itp in range(idaapi.ITP_SEMI, idaapi.ITP_COLON):
+        tl.itp = itp    
+        cfunc.set_user_cmt(tl, comment)
+        cfunc.save_user_cmts()
+        #apparently you have to cast cfunc to a string, to make it update itself
+        unused = cfunc.__str__()
+        if not cfunc.has_orphan_cmts():
+            commentSet = True
+            cfunc.save_user_cmts()
+            break
+        cfunc.del_orphan_cmts()
+
+    if not commentSet:
+        print "[ERROR] Please set \"%s\" to line %s manually" % (comment, hex(int(address)))
+
+
+def decryptAllReferencesToCryptFunction(cryptFunctionAddress):
+    cryptHelper = crypt()
+    for addr in idautils.CodeRefsTo(cryptFunctionAddress,0):
+        #find pushes from xrefs backwards to get the function's argument
+        offset = getOperandBackwards(addr, 10)
+        if offset == -1:
+            print "[-] Error at %s - did not find correct byte" % hex(int(addr))
+        else:
+            strFromPtr = getStringFromPointer(offset)
+            decryptedString = cryptHelper.decrypt(strFromPtr)
+            setCommentToDecompilation(decryptedString, addr)#XXX TODO
+            print "[+] Decrypted string at Addr: %s Offset: %s Crypted: %s Decrypted: %s" % (hex(int(addr)), offset, strFromPtr, decryptedString)
+
+if __name__ == "__main__":
+    decryptAllReferencesToCryptFunction(0x00405210)
+    print "----------------"
+
+    decryptAllReferencesToCryptFunction(0x004019F0)
+    print "----------------"
+
+    decryptAllReferencesToCryptFunction(0x00407110)
+    print "----------------"
+
+    #get check IPs and domains to check
+    base = 0x00425358
+    cryptHelper = crypt()
+    while True:    
+        offset =  Dword(base)
+        if offset == 0:
+            break    
+        strFromPtr = getStringFromPointer(offset)
+        print "[+] Decrypted string at Addr: %s Offset: %s Crypted: %s Decrypted: %s" % (hex(int(base)), offset, strFromPtr, cryptHelper.decrypt(strFromPtr))
+        base += 4
+    print "----------------"
+
+    #get paths for checking IPs and domains
+    base = 0x0042538C
+    for i in range(0x0042538C, 0x004253B8, 4):    
+        offset =  Dword(i)
+        if offset == 0:
+            continue    
+        strFromPtr = getStringFromPointer(offset)    
+        print "[+] Decrypted string at Addr: %s Offset: %s Crypted: %s Decrypted: %s" % (hex(int(i)), offset, strFromPtr, cryptHelper.decrypt(strFromPtr))
+    print "----------------"
+
